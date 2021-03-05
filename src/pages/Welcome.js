0894e7da74421fdb3c02d2a4649fff0abf453144
Michael Grigoryan <michael.grigoryan25@gmail.com>
2021-03-05 14:31:10 +0400

    Minor changes

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,53 +1,52 @@
-import React from "react";
 import { Center } from "@chakra-ui/react";
 import { Helmet } from "react-helmet-async";
 
 import Auth from "../components/Auth/index";
 
-export default class Welcome extends React.Component {
-  render() {
-    return (
-      <Center>
-        <Helmet>
-          {/* Main Meta Tags */}
-          <title>Welcome to Usocial</title>
-          <meta name="language" content="English" />
-          <meta property="locale" content="en_US" />
-          <meta name="robots" content="index, follow" />
-          <meta name="revisit-after" content="1 days" />
-          <meta name="title" content="Welcome to Usocial" />
-          <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-          <meta
-            name="description"
-            content="Usocial is a next-gen privacy oriented social media networking platform."
-          />
-          <meta
-            name="keywords"
-            content="social media website, social, media, videos, images, text, comment"
-          />
+const Welcome = () => {
+  return (
+    <Center>
+      <Helmet>
+        {/* Main Meta Tags */}
+        <title>Welcome to Usocial</title>
+        <meta name="language" content="English" />
+        <meta property="locale" content="en_US" />
+        <meta name="robots" content="index, follow" />
+        <meta name="revisit-after" content="1 days" />
+        <meta name="title" content="Welcome to Usocial" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta
+          name="description"
+          content="Usocial is a next-gen privacy oriented social media networking platform."
+        />
+        <meta
+          name="keywords"
+          content="social media website, social, media, videos, images, text, comment"
+        />
 
-          {/* Open Graph Meta Tags */}
-          <meta property="og:type" content="website" />
-          <meta property="og:locale" content="en_US" />
-          <meta property="og:site_name" content="Usocial" />
-          <meta property="og:title" content="Welcome to Usocial" />
-          <meta
-            property="og:description"
-            content="Usocial is a next-gen privacy oriented social media networking platform."
-          />
+        {/* Open Graph Meta Tags */}
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:site_name" content="Usocial" />
+        <meta property="og:title" content="Welcome to Usocial" />
+        <meta
+          property="og:description"
+          content="Usocial is a next-gen privacy oriented social media networking platform."
+        />
 
-          {/* Twitter Meta Tags */}
-          <meta property="twitter:type" content="website" />
-          <meta property="twitter:locale" content="en_US" />
-          <meta property="twitter:site_name" content="Usocial" />
-          <meta property="twitter:title" content="Welcome to Usocial" />
-          <meta
-            property="twitter:description"
-            content="Usocial is a next-gen privacy oriented social media networking platform."
-          />
-        </Helmet>
-        <Auth />
-      </Center>
-    );
-  }
-}
+        {/* Twitter Meta Tags */}
+        <meta property="twitter:type" content="website" />
+        <meta property="twitter:locale" content="en_US" />
+        <meta property="twitter:site_name" content="Usocial" />
+        <meta property="twitter:title" content="Welcome to Usocial" />
+        <meta
+          property="twitter:description"
+          content="Usocial is a next-gen privacy oriented social media networking platform."
+        />
+      </Helmet>
+      <Auth />
+    </Center>
+  );
+};
+
+export default Welcome;
